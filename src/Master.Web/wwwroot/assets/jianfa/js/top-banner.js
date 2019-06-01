c85bbf62386122f4b4d15a8ee3daacbb961dc750
ray <goddess0910kk@gmail.com>
2019-06-01 23:21:38 +0800

    bundle有错误

---
+++ src/Master.Web/wwwroot/assets/jianfa/js/top-banner.js
@@ -0,0 +1,19 @@
+var speed = 0;
+var scrollTop = null;
+//var hold = 0;
+var jianfa_top;
+var pos = null;
+//var timer = null;
+var moveHeight = null;
+jianfa_top = document.getElementById("jianfa_top");
+window.onscroll=scroll_ad;
+function scroll_ad(){
+ scrollTop = document.documentElement.scrollTop+document.body.scrollTop;
+ pos = scrollTop - jianfa_top.offsetTop;
+ pos = pos
+ moveHeight = pos>0?Math.ceil(pos):Math.floor(pos);
+ if(moveHeight!=0){
+  jianfa_top.style.top = jianfa_top.offsetTop+moveHeight+"px";
+  setTimeout(scroll_ad,speed);
+ }
+}
