9eb8878f21ae1826fe0feb793ae4eb0f12cd7780
Tiansfather <Tiansfather@TIANSFATHER>
2019-06-04 12:28:36 +0800

    abp.message引入iview弹窗

--- src/Master.Web/wwwroot/assets/js/home.js
+++ src/Master.Web/wwwroot/assets/js/home.js
@@ -1,41 +1,66 @@
 ﻿; (function () {
-    abp.message.info = function (message, title) {
-        app.$Message.info({ content: message });
+    abp.message.info = function (message, title, callback) {
+        return $.Deferred(function ($dfd) {
+            app.$Modal.info({
+                title: title || '消息', content: message, loading: false, onOk: function () {
+                    callback && callback();
+                    $dfd.resolve();
+                }
+            })
+        });
     };
 
-    abp.message.success = function (message, title) {
-        app.$Message.success({ content: message });
+    abp.message.success = function (message, title,callback) {
+        return $.Deferred(function ($dfd) {
+            app.$Modal.success({
+                title: title || '成功', content: message, loading: false, onOk: function () {
+                    callback && callback();
+                    $dfd.resolve();
+                }
+            })
+        });
+        //app.$Modal.success({ title: title||'成功',content: message });
     };
 
-    abp.message.warn = function (message, title) {
-        app.$Message.warning({ content: message });
+    abp.message.warn = function (message, title, callback) {
+        return $.Deferred(function ($dfd) {
+            app.$Modal.warning({
+                title: title || '警告', content: message, loading: false, onOk: function () {
+                    callback && callback();
+                    $dfd.resolve();
+                }
+            })
+        });
     };
 
-    abp.message.error = function (message, title) {
-        app.$Message.error({ content: message });
+    abp.message.error = function (message, title, callback) {        
+        return $.Deferred(function ($dfd) {
+            app.$Modal.error({
+                title: title || '错误', content: message, loading: false, onOk: function () {
+                    callback && callback();
+                    $dfd.resolve();
+                }
+            })
+        });
     };
 
-    //abp.message.confirm = function (message, titleOrCallback, callback) {
-    //    var opts = $.extend(
-    //        {},
-    //        abp.libs.layer.config['default'],
-    //        abp.libs.layer.config.confirm
-    //    );
-
-    //    if ($.isFunction(titleOrCallback)) {
-    //        callback = titleOrCallback;
-    //    } else if (titleOrCallback) {
-    //        opts.title = titleOrCallback;
-    //    };
+    abp.message.confirm = function (message, titleOrCallback,loading, callback) {
+        var title = "";
+        if ($.isFunction(titleOrCallback)) {
+            callback = titleOrCallback;
+        } else if (titleOrCallback) {
+            title = titleOrCallback;
+        };
 
 
-    //    return $.Deferred(function ($dfd) {
-    //        layui.layer.confirm(message, opts, function (index) {
-    //            layer.close(index);
-    //            callback && callback();
-    //            $dfd.resolve();
-    //        })
-    //    });
-    //};
+        return $.Deferred(function ($dfd) {
+            app.$Modal.confirm({
+                title: title, content: message, loading: loading, onOk: function () {
+                    callback && callback();
+                    $dfd.resolve();
+                }
+            })
+        });
+    };
 
 })();
