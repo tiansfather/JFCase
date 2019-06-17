53b032ab1ff22075e3658ee1c0737918f15a0c47
Tiansfather <Tiansfather@TIANSHOUSE>
2019-06-18 00:25:25 +0800

    上传大小及进度修正

--- src/Master.Web/Startup/Program.cs
+++ src/Master.Web/Startup/Program.cs
@@ -8,7 +8,7 @@ namespace Master.Web.Startup
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
+                .UseKestrel(options=>options.Limits.MaxRequestBodySize=null)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
