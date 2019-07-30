a5a776796798b792666286ae3b808ae4aba9cbf6
Tiansfather <Tiansfather@TIANSFATHER>
2019-07-30 15:33:39 +0800

    1

--- src/Common/ZipHelper.cs
+++ src/Common/ZipHelper.cs
@@ -13,18 +13,21 @@ namespace Common
         /// </summary>
         /// <param name="targetFile">解压文件路径</param>
         /// <param name="zipFile">解压文件后路径</param>
-        public static void Decompression(string targetFile, string zipFile)
+        public static List<string> Decompression(string targetFile, string zipFile)
         {
+            var result = new List<string>();
             using (var archive = ArchiveFactory.Open(targetFile))
             {
                 foreach (var entry in archive.Entries)
                 {
                     if (!entry.IsDirectory)
                     {
+                        result.Add(entry.Key);
                         entry.WriteToDirectory(zipFile);
                     }
                 }
-            }                
+            }
+            return result;
         }
 
         public static IEnumerable<string> GetFileNames(string targetFile)
