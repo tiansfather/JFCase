41323645799b945ab0331ad946a369ea3e01ff09
Tiansfather <Tiansfather@TIANSHOUSE>
2019-11-20 21:23:27 +0800

    1

--- src/Master.Application/Users/LawerAppService.cs
+++ src/Master.Application/Users/LawerAppService.cs
@@ -1,13 +1,21 @@
 ﻿using Master.Authentication;
+using Master.Dto;
 using Master.Entity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Master.Users
 {
     public class LawerAppService:MasterAppServiceBase<User,long>
     {
+        protected override async Task<IQueryable<User>> GetQueryable(RequestPageDto request)
+        {
+            return (await base.GetQueryable(request)).IgnoreQueryFilters().Where(o=>!o.IsDeleted && o.TenantId!=null);
+        }
         protected override object PageResultConverter(User entity)
         {
             return new
